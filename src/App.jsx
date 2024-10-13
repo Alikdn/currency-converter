630ef7e723baa80bc6c539f9f075cf74b926c97b
ALI Khaldoun <ali.khaldoun03@gmail.com>
2024-10-13 02:54:09 +0100

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   // Fetch exchange rates
   useEffect(() => {
-    fetch(`https://v6.exchangerate-api.com/v6/70164fa06850c06e4771cf48/latest/${fromCurrency}`)
+    fetch(`https://v6.exchangerate-api.com/v6/70164fa06850c06e4771cf48SS/latest/${fromCurrency}`)
       .then(response => response.json())
       .then(data => {
         if (data.result !== 'success') throw new Error('Failed to fetch data');
